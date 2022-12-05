bdab91023e224d4def60e75b46fc65b7da90c410
TheoPierne <theo.pierne2@gmail.com>
2022-12-05 16:46:45 +0100

    fix: fix navbar structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <script setup>
-import { useSessionStore } from './stores/session';
-const session = new useSessionStore();
+  import { useSessionStore } from './stores/session';
+  const session = new useSessionStore();
 </script>
 
 <template>
   <div class="container">
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <div class="navbar-menu">
-          <div class="navbar-start">
-            <RouterLink to="/" class="navbar-item">CO'OP</RouterLink>
-          </div>
-        </div>
+        <RouterLink to="/" class="navbar-item">CO'OP</RouterLink>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+
       </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <RouterLink v-if="session.data.token" to="/logout" class="button is-danger">Se déconnecter</RouterLink>
-            <RouterLink v-else="session.data.token" to="/login" class="button is-primary">Se connecter</RouterLink>
-            <RouterLink to="/register" class="button is-light">S'inscrire</RouterLink>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <RouterLink v-if="session.data.token" to="/logout" class="button is-danger">Se déconnecter</RouterLink>
+              <RouterLink v-else="session.data.token" to="/login" class="button is-primary">Se connecter</RouterLink>
+              <RouterLink to="/register" class="button is-light">S'inscrire</RouterLink>
+            </div>
           </div>
         </div>
       </div>

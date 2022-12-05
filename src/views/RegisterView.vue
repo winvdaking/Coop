77c2c0716b3d30f062d2b0215793a23d307d5929
TheoPierne <theo.pierne2@gmail.com>
2022-12-05 17:28:27 +0100

    feat: ajout d'une alerte en cas d'erreur

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,13 @@
 <script setup>
 
   import { useRouter } from 'vue-router';
+  import { onMounted } from 'vue';
+
+  onMounted(() => {
+    const externalScript = document.createElement('script')
+    externalScript.setAttribute('src', '//cdn.jsdelivr.net/npm/sweetalert2@11')
+    document.body.appendChild(externalScript)
+  });
 
   const router = new useRouter();
 
@@ -14,10 +21,7 @@
     fullname: false,
     email: false,
     password: false
-  })
-
-  let isErrorOnRegister = ref(false);
-  let errorMessage = ref('');
+  });
 
   function preventForm() {
 
@@ -36,11 +40,16 @@
         formError.value.password = password === '';
 
         if (fullname && email && password) {
-          isErrorOnRegister.value = true;
-          errorMessage.value = d.message;
-          setTimeout(() => {
-            isErrorOnRegister.value = false
-          }, 2500);
+          Swal.fire({
+            width: '28em',
+            position: 'bottom-end',
+            icon: 'error',
+            text: d.message,
+            showConfirmButton: false,
+            timer: 3500,
+            backdrop: false,
+            toast: true
+          });
         }
 
       } else {
@@ -52,74 +61,78 @@
 
     })
     .catch(e => {
-
       console.log(e)
-      isErrorOnRegister.value = true;
-      errorMessage.value = e.toString();
-      setTimeout(() => {
-        isErrorOnRegister.value = false
-      }, 1500);
-
+      Swal.fire({
+        width: '28em',
+        position: 'bottom-end',
+        icon: 'error',
+        text: e.toString(),
+        showConfirmButton: false,
+        timer: 3500,
+        backdrop: false,
+        toast: true
+      });
     });
 
   }
 </script>
 
 <template>
-  <form @submit.prevent="preventForm">
-    <div class="field">
-      <label class="label">Nom/Pseudo</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input" :class="{'is-danger': formError.fullname}" type="text" v-model.trim="member.fullname" name="fullname" placeholder="toto">
-        <span class="icon is-small is-left">
-          <i class="fas fa-user"></i>
-        </span>
-        <span class="icon is-small is-right" :class="{'is-hidden': !formError.fullname}">
-          <i class="fas fa-exclamation-triangle"></i>
-        </span>
-      </div>
-      <p class="help is-danger" :class="{'is-hidden': !formError.fullname}">Le nom/pseudo est invalide.</p>
-    </div>
+  <div class="columns">
+    <div class="column is-4 is-offset-4 box">
+      <h2 class="title">S'inscrire</h2>
+      <form @submit.prevent="preventForm">
+        <div class="field">
+          <label class="label">Nom/Pseudo</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" :class="{'is-danger': formError.fullname}" type="text" v-model.trim="member.fullname" name="fullname" placeholder="toto">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right" :class="{'is-hidden': !formError.fullname}">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+          <p class="help is-danger" :class="{'is-hidden': !formError.fullname}">Le nom/pseudo est invalide.</p>
+        </div>
 
-    <div class="field">
-      <label class="label">Email</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input" :class="{'is-danger': formError.email}" type="email" v-model.trim="member.email" name="email" placeholder="toto@gmail.com">
-        <span class="icon is-small is-left">
-          <i class="fas fa-envelope"></i>
-        </span>
-        <span class="icon is-small is-right" :class="{'is-hidden': !formError.email}">
-          <i class="fas fa-exclamation-triangle"></i>
-        </span>
-      </div>
-      <p class="help is-danger" :class="{'is-hidden': !formError.email}">L'adresse email est invalide.</p>
-    </div>
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" :class="{'is-danger': formError.email}" type="email" v-model.trim="member.email" name="email" placeholder="toto@gmail.com">
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right" :class="{'is-hidden': !formError.email}">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+          <p class="help is-danger" :class="{'is-hidden': !formError.email}">L'adresse email est invalide.</p>
+        </div>
 
-    <div class="field">
-      <label class="label">Mot de passe</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input" :class="{'is-danger': formError.password}" type="password" v-model.trim="member.password" name="password" placeholder="****************">
-        <span class="icon is-small is-left">
-          <i class="fas fa-lock"></i>
-        </span>
-        <span class="icon is-small is-right" :class="{'is-hidden': !formError.password}">
-          <i class="fas fa-exclamation-triangle"></i>
-        </span>
-      </div>
-      <p class="help is-danger" :class="{'is-hidden': !formError.password}">Le mot de passe est invalide.</p>
-    </div>
+        <div class="field">
+          <label class="label">Mot de passe</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" :class="{'is-danger': formError.password}" type="password" v-model.trim="member.password" name="password" placeholder="****************">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+            <span class="icon is-small is-right" :class="{'is-hidden': !formError.password}">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+          <p class="help is-danger" :class="{'is-hidden': !formError.password}">Le mot de passe est invalide.</p>
+        </div>
 
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-link">S'inscrire</button>
-      </div>
-      <div class="control">
-        <RouterLink class="button is-link is-light" to="/">Annuler</RouterLink>
-      </div>
-    </div>
-  </form>
-  <div class="notification is-danger" :class="{'is-hidden': !isErrorOnRegister}">
-    <button class="delete"></button>
-    {{ errorMessage }}
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-primary">S'inscrire</button>
+          </div>
+          <div class="control">
+            <RouterLink class="button is-primary is-light" to="/">Annuler</RouterLink>
+          </div>
+        </div>
+      </form>
+    </div> 
   </div>
 </template>
